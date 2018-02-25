13e35aba22880867621f6b8b63fa8a8aa3d6d25b
Pegah Eizad <peizadkh@gmail.com>
2018-02-25 15:25:04 -0800

    Created data file.

---
+++ dataFile.js
@@ -0,0 +1,27 @@
+const dataArray = [
+    {
+      question: 'How patient are you in learning?',
+      answers: [
+        'I want life to have one big EASY button.',
+        'The reasonably minimum learning I get away with is fine. by me.',
+        'I don\'t mind a challenge every now and then.',
+        'I pride myself on my attack of difficult subjects.',
+        'I prefer the extensive training of Zen Master before I venture out into the world.'
+        ],
+      feedback: 'Leave no trace',
+      alt: 'tent icon'
+    },
+    {
+      question: 'How much of a gamer are you?',
+      answers: [
+        'Games are my whole life!',
+        'I just wanna have fun!',
+        'Meh, games are OK, I guess.',
+        'Sure, I like some games as long as they don\'t get in my way',
+        'The only time I game is when I play Tetris while waiting for some other process to finish.'
+        ],
+        feedback: 'feedback',
+        icon: 'https://i.imgur.com/iQ0FBhB.png',
+        alt: 'shield for 10 essentials icon'
+    }
+];

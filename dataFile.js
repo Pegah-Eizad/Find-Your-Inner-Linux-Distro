c6872f206f74f0b8afa9264575a843b2e02c7e44
Pegah Eizad <peizadkh@gmail.com>
2018-03-02 19:34:42 -0800

    Changed structure of data file
    Data file structure changed to better suit 'personality quiz'-style functionality.

--- dataFile.js
+++ dataFile.js
@@ -1,27 +1,77 @@
-const dataArray = [
+var model = {
+    currentQuestion: 0,
+    totalWeight: 0
+};
+
+const results = [
     {
-      question: 'How patient are you in learning?',
-      answers: [
-        'I want life to have one big EASY button.',
-        'The reasonably minimum learning I get away with is fine. by me.',
-        'I don\'t mind a challenge every now and then.',
-        'I pride myself on my attack of difficult subjects.',
-        'I prefer the extensive training of Zen Master before I venture out into the world.'
-        ],
-      feedback: 'Leave no trace',
-      alt: 'tent icon'
+        min:0,
+        max:10,
+        distro: 'Ubuntu'
     },
     {
-      question: 'How much of a gamer are you?',
-      answers: [
-        'Games are my whole life!',
-        'I just wanna have fun!',
-        'Meh, games are OK, I guess.',
-        'Sure, I like some games as long as they don\'t get in my way',
-        'The only time I game is when I play Tetris while waiting for some other process to finish.'
-        ],
-        feedback: 'feedback',
-        icon: 'https://i.imgur.com/iQ0FBhB.png',
-        alt: 'shield for 10 essentials icon'
+        min:11,
+        max:20,
+        distro: 'Redhat'
     }
+]
+
+const dataArray = [ 
+    { 
+        question: 'How patient are you in learning?', 
+        answers: [ 
+            {
+                text: 'I want life to have one big EASY button.', 
+                weight: 5
+            }, 
+            {
+                text: 'The reasonably minimum learning I get away with is fine. by me.',
+                weight: 2
+            },
+            {
+                text: 'I don\'t mind a challenge every now and then.', 
+                weight: 7
+            },
+            {
+                text: 'I pride myself on my attack of difficult subjects.', 
+                weight: 4
+            },
+            {
+                text:'I prefer the extensive training of Zen Master before I venture out into the world.',
+                weight:8
+            }
+        ], 
+        feedback: 'Some Feedback 1', 
+        alt: 'tent icon' 
+    }, 
+    
+    { 
+      question: 'How much of a gamer are you?', 
+      answers: [
+            { 
+              text: 'Games are my whole life!', 
+               weight: 5
+            },
+            {
+               text:'I just wanna have fun!', 
+               weight: 2
+            },
+            {
+               text: 'Meh, games are OK, I guess.', 
+               weight: 7
+            },
+            {
+               text: 'Sure, I like some games as long as they don\'t get in my way', 
+               weight: 4
+            },
+            {
+               text: 'The only time I game is when I play Tetris while waiting for some other process to finish.' ,
+               weight: 8
+            }
+    ], 
+    feedback: 'feedback', 
+    icon: 'https://i.imgur.com/iQ0FBhB.png', 
+    alt: 'shield for 10 essentials icon' 
+  } 
+
 ];

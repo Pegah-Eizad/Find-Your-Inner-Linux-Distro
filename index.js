67385edf33c08ed3d1c451ff5acb2a43d116d4d7
Pegah Eizad <peizadkh@gmail.com>
2018-03-04 18:00:08 -0800

    Added quiz questions rendering
    Added functions to render quiz questions. 
    Dynamically adds form to display answer choices. 
    Questions appears in shell.

--- index.js
+++ index.js
@@ -20,52 +20,114 @@ function introduceQuiz(){
     }
     else{
       //stop
-      $('.user-feedback').append("$ Goodbye.");
+      $('.user-feedback').append('<p class="user-feedback">$ Goodbye!</p>');
     }
+    event.stopPropegation();
   }); 
   //alert('Get user answer finished');
 }
 
 
-
-function generateQuestions(){
-  console.log('testing generateQuestions');
-  for(let i=0; i<dataArray.length; i++)
-  {
-     
-  }
-
-}
-
-//starts quiz by clearing start page,
-//adding quiz elements,
-//and generating questions
 function startQuiz()
 {   
     console.log('inside startQuiz');
     //clear start page 
     $('.user-feedback').remove();
-    $('.line-1').remove();
-    //move the shell up 
-    $('.text-editor-wrap').css({"top":"50px"});
-    //Add quiz divs to the page 
-    $('.question-div').addClass('question-box');
-    setTimeout(function afterOneSecond(){
-       $('.penguin-div').addClass('penguin-box');
-       //generate questions ?? 
-    }, 1000);
+    //$('p.questions').removeClass('start-quiz-question');
+    $('.questions').remove();
+    //move the shell up
+    $('.text-editor-start').addClass('text-editor').removeClass('text-editor-start');
+    renderNextQuestion();
+    //setTimeout(function afterOneSecond(){
+       
+    //}, 1000);
 }
 
+function generateQuestion(){
+  console.log('testing generateQuestion');
+  if (questionNum < dataArray.length) {
+     //$('.questions').append(${dataArray[questionNum].question});
+     $('div.text-body').append(`<p class="questions">${dataArray[questionNum].question}</p>`);
+  }
+}
+function generateAnswers(){
+     //${STORE[questionNumber].question}
+     if (questionNum < dataArray.length) {
+       return `<div class="question-${questionNum}">
+       <form>
+       <fieldset>
+       <label class="answerChoice">
+       <input type="radio" value="${dataArray[questionNum].answers[0].text}" name="answer" required>
+       <span>${dataArray[questionNum].answers[0].text}</span>
+       </label>
+       <label class="answerChoice">
+       <input type="radio" value="${dataArray[questionNum].answers[1].text}" name="answer" required>
+       <span>${dataArray[questionNum].answers[1].text}</span>
+       </label>
+       <label class="answerChoice">
+       <input type="radio" value="${dataArray[questionNum].answers[2].text}" name="answer" required>
+       <span>${dataArray[questionNum].answers[2].text}</span>
+       </label>
+       <label class="answerChoice">
+       <input type="radio" value="${dataArray[questionNum].answers[3].text}" name="answer" required>
+       <span>${dataArray[questionNum].answers[3].text}</span>
+       </label>
+       <label class="answerChoice">
+       <input type="radio" value="${dataArray[questionNum].answers[4].text}" name="answer" required>
+       <span>${dataArray[questionNum].answers[4].text}</span>
+       </label>
+       <button type="submit" class="submitButton">Submit</button>
+       </fieldset>
+       </form>
+       </div>`;
+  }
+}
 
+function updateQuestionNumber(){
+  console.log('Inside Questions num. questionNum is: ' + questionNum + 'updating...');
+  questionNum++;
+  console.log('new questionNum is: ' + questionNum);
+}
+
+function renderNextQuestion(){
+  if (questionNum === 0){
+    console.log('inside renderNextQuestion with questionNum 0');
+    displayQuestionAndAnswers();
+  }
+  else{
+    console.log('inside renderNextQuestion and questionNum is not 0!')
+    $('.submitButton').on('click', function(event){
+      updateQuestionNumber();
+         displayQuestionAndAnswers();   
+    });
+}
+}
+
+function displayQuestionAndAnswers(){
+  console.log('inside displayQuestionAndAnswers');
+  propStopped();
+  generateQuestion();
+  setTimeout(function afterThreeSeconds(){
+   $('main').append(generateAnswers());
+  }, 3000);
+}
+
+function propStopped( event ) {
+  var msg = "";
+  if ( event.isPropagationStopped() ) {
+    msg = "called";
+  } else {
+    msg = "not called";
+  }
+  console.log(msg);
+}
 //Create Quiz
 function quizApp(){
   console.log('inside quizApp.');
   //get user answer
   introduceQuiz();
-  
 }
 
 $(quizApp);
-
 
 

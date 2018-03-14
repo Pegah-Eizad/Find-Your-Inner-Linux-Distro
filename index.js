40f3c9724003b02d815e7a73bca7c36eeea4cd18
Pegah Eizad <peizadkh@gmail.com>
2018-03-13 21:06:32 -0700

    Fixed next-button functionality
    Quiz now flows through all questions. 
    Previously was not detaching 'click' event from document.body.

--- index.js
+++ index.js
@@ -9,9 +9,10 @@ function introduceQuiz(){
     $('.output').text(`Key pressed: ${event.key}`)
   );
   */
-
+  $('input').focus();
   $('input').on('keydown', function (event){
     var keycode = (event.keyCode ? event.keyCode : event.which);
+    $('input').blur();
     console.log('key pressed!! ' + event.keyCode);
     if (event.which == 121 || event.which == 13){
       console.log('it worked');
@@ -27,17 +28,7 @@ function introduceQuiz(){
     }
     //testing
     event.stopPropagation();
-    function propStopped( event ) {
-    var msg = "";
-    if ( event.isPropagationStopped() ) {
-      msg = "CALLED";
-    } else {
-      msg = "NOT CALLED";
-    }
-    console.log(msg);
-  }
   }); 
-  //alert('Get user answer finished');
 }
 
 function startQuiz()
@@ -50,7 +41,7 @@ function startQuiz()
     $('input').remove();
     //move the shell up
     $('.text-editor-start').addClass('text-editor').removeClass('text-editor-start');
-    displayQuestionAndAnswers();
+    renderQuestion();
 }
 
 function updateQuestionNumber(){
@@ -59,24 +50,70 @@ function updateQuestionNumber(){
   console.log('new questionNum is: ' + questionNum);
 }
 
-function renderNextQuestion(){
-  console.log('inside renderNextQuestion');
-  $(document).on('click','.submitButton',function(e){
+function answerSubmit(){
+  console.log('inside answerSubmit');
+  $(document.body).on('submit', '#question-form', function(e){
     e.preventDefault();
     console.log('Default Preveted!!!!!');
-    $('.questions').remove();
-    $('.answers').remove();
-    updateQuestionNumber();
-    displayQuestionAndAnswers();
+    //give feedback
+    giveFeedback();
+    //render nextQuestion
 }); 
 }
 
-function displayQuestionAndAnswers(){
-  console.log('inside displayQuestionAndAnswers');
+function giveFeedback(){
+   
+  $(document.body).unbind('submit'); 
+  let answerSelected = document.querySelector('input[name="answer"]:checked').value;
+  let answerSelectedId = document.querySelector('input[name="answer"]:checked'.id);
+  let x = 0;
+  switch (answerSelectedId){
+    case 'selector1':  x=1; break;
+    case 'selector2':  x=2; break;
+    case 'selector3':  x=3; break;
+    case 'selector4':  x=4; break;
+  }
+  //clear page content
+  $('.questions').remove();
+  $('.answers').remove();
+  $('.text-editor').remove();
+  //display feedback content  
+  //dataArray[questionNum].answers[$('input[name=answer]:checked').val()]
+  $('main').append(`<div class="user-answer">${answerSelected}</div>
+                     <div class="answer-feedback"> ${dataArray[questionNum].answers[x].feedback} </div>
+                     <button type="button" class="next-button">Next</button>`);
+
+}
+
+
+function nextClick(){
+  $(document.body).on('click', '.next-button', function(e){
+    $('.user-answer').remove();
+    $('.answer-feedback').remove();
+    $('.next-button').remove();
+    //$(document.body).bind('submitButt')
+    let textEditorDiv = `<div class="text-editor">
+        <div class="title-bar"><span class="title">typed.js &mdash; bash &mdash; 80x<span class="terminal-height">25</span></span></div>
+        <div class="text-body">
+          <span class="type-writer-constant-texts"> Last login: Sat Feb 24 14:36:27 on tt </span>
+          <br> 
+          <span class="type-writer-constant-texts">$</span>
+        </div>
+    </div>`;
+    $('main').append(textEditorDiv);
+    updateQuestionNumber();
+    renderQuestion();
+    answerSubmit();
+  });
+}
+
+
+function renderQuestion(){
+  console.log('inside renderQuestion');
   generateQuestion();
-  setTimeout(function afterThreeSeconds(){
+  setTimeout(function afterOneSecond(){
    $('main').append(generateAnswers());
-  }, 2000);
+  }, 1500);
 }
 
 function generateQuestion(){
@@ -86,33 +123,28 @@ function generateQuestion(){
      $('div.text-body').append(`<p class="questions">${dataArray[questionNum].question}</p>`);
   }
 }
+
+/*
+    <input id="selector0" type="radio" name="select" value="M">
+    <label for="selector0">Male</label>
+*/
 function generateAnswers(){
      //${STORE[questionNumber].question}
      console.log('inside generate answers with questionnum' + questionNum);
      if (questionNum < dataArray.length) {
        return `<div class="answers">
-       <form>
-       <fieldset>
-       <label class="answerChoice">
-       <input type="radio" value="${dataArray[questionNum].answers[0].text}" name="answer" required>
-       <span>${dataArray[questionNum].answers[0].text}</span>
-       </label>
-       <label class="answerChoice">
-       <input type="radio" value="${dataArray[questionNum].answers[1].text}" name="answer" required>
-       <span>${dataArray[questionNum].answers[1].text}</span>
-       </label>
-       <label class="answerChoice">
-       <input type="radio" value="${dataArray[questionNum].answers[2].text}" name="answer" required>
-       <span>${dataArray[questionNum].answers[2].text}</span>
-       </label>
-       <label class="answerChoice">
-       <input type="radio" value="${dataArray[questionNum].answers[3].text}" name="answer" required>
-       <span>${dataArray[questionNum].answers[3].text}</span>
-       </label>
-       <label class="answerChoice">
-       <input type="radio" value="${dataArray[questionNum].answers[4].text}" name="answer" required>
-       <span>${dataArray[questionNum].answers[4].text}</span>
-       </label>
+       <form id="question-form">
+       <fieldset>   
+       <input id="selector0" type="radio" value="${dataArray[questionNum].answers[0].text}" name="answer" required> 
+       <label for="selector0" class="answerChoice1"><span>${dataArray[questionNum].answers[0].text}</span></label>
+       <input id="selector1" type="radio" value="${dataArray[questionNum].answers[1].text}" name="answer" required>
+       <label for="selector1" class="answerChoice2"><span>${dataArray[questionNum].answers[1].text}</span></label>
+       <input id="selector2" type="radio" value="${dataArray[questionNum].answers[2].text}" name="answer" required>
+       <label for="selector2" class="answerChoice3"><span>${dataArray[questionNum].answers[2].text}</span></label>
+       <input id="selector3" type="radio" value="${dataArray[questionNum].answers[3].text}" name="answer" required>
+       <label  for="selector3" class="answerChoice4"><span>${dataArray[questionNum].answers[3].text}</span></label>
+       <input id="selector4" type="radio" value="${dataArray[questionNum].answers[4].text}" name="answer" required>
+       <label for="selector4" class="answerChoice5"><span>${dataArray[questionNum].answers[4].text}</span></label>
        <button type="submit" class="submitButton">Submit</button>
        </fieldset>
        </form>
@@ -123,6 +155,8 @@ function generateAnswers(){
 $(document).ready(function(){
   console.log('inside quizApp.');
   introduceQuiz();
-  renderNextQuestion();
+  answerSubmit();
+  nextClick();
   //get user answer 
 });
+
